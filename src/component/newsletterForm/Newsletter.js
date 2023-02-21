febe182d451e6fed56b0ccd9ca451b18d06a4ba3
unknown <gbengalilmahn@gmail.com>
2023-02-21 12:08:37 +0100

    chatbot

--- src/component/newsletterForm/Newsletter.js
+++ src/component/newsletterForm/Newsletter.js
@@ -1,26 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./newsletter.scss"
 import emailjs from 'emailjs-com';
 import { Button, TextField } from '@mui/material';
 
 export default function Newsletter() {
 
+    const [showMessage, setShowMessage]= useState(false)
+    const [message, setMessage]= useState("")
+
+    
     function sendEmail(e) {
         e.preventDefault();    //This is important, i'm not sure why, but the email won't send without it
     
         emailjs.sendForm('service_9hc60zg', 'template_racbhye', e.target, '1C9x_ChYylYmwyitQ')
           .then((result) => {
-              window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior) 
-          }, (error) => {
+              setShowMessage(true)
+              setMessage("Thank You for subscribing to our Newsletter")
+              setInterval(() => {
+               setShowMessage(false)
+               //    to reload after success message 
+                  window.location.reload()
+           }, 3000);
+        }, (error) => {
               console.log(error.text);
+              setMessage("Error, Try again")
           });
       }
 
   return (
     <>
     <h4>To receive updates, kindly subscribe to our Newsletter</h4>
+    {showMessage && <p className='message'>{message}</p>}
     <form className='newsletter' onSubmit={sendEmail}>
-        <TextField id="outlined-basic" label="Enter your Email" variant="outlined" required type="email" name="email" className='input'/>
+        <TextField id="outlined-basic" label="Enter your Email" variant="outlined" required type="email" name="email" className='input' />
         <Button type="submit"  variant="contained" className="appointment-btn">Subscribe</Button>
     </form>
     </>
